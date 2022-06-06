f46e4f38cfda96562d09b7b545a2514fcaafee8d
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-06-05 21:08:44 -0500

    creates comments model

---
+++ models/comment.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  comment.init({
+    userId: DataTypes.INTEGER,
+    vehicleId: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'comment',
+  });
+  return comment;
+};
